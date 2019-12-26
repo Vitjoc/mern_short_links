97f27d54493500d373eccae7c7c8e5f798e9f311
Biktop <vik.work.001@gmail.com>
2019-12-26 06:18:05 +0200

    basic express and mongodb congig

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("app.js - message 2");
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () =>
+      console.log(`App had been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
